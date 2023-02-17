d4433885b6ea97bfd1b70b373c6b01890930c9ea
surajtgoud <78561890+surajtgoud@users.noreply.github.com>
2023-02-17 22:46:46 +0530

    added templating engine handlebars

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
+const path = require("path");
 
 const app = express();
 
 const PORT = 3000;
+
+app.set("view enginer", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 //Middleware in action
 app.use((req, res, next) => {
@@ -13,6 +17,13 @@ app.use((req, res, next) => {
 });
 //serving static site from express
 app.use("/site", express.static("public"));
+//serving hbs tenplate
+app.use("/", (req, res) => {
+  app.render("index", {
+    title: "This Site Is Served By Express",
+    caption: "This Site Is Served By Express",
+  });
+});
 //Middleware for parsing req.body json
 app.use(express.json());
 
